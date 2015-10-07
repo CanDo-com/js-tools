5977e9657880cf7c66685f3c843c23b28b5c85ef
Sergiy Misyura <sergey@cando.com>
2015-10-07 12:34:24 -0400

    composer.json and Yii asset bundle has been added

---
+++ Asset.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.cando.com/
+ * @copyright Copyright (c) 2015 Advertical LLC dba CanDo
+ */
+
+namespace cando\jstools;
+
+use \yii\web\AssetBundle;
+
+class JSToolsAsset extends AssetBundle
+{
+	public function init()
+	{
+		if (YII_ENV_DEV)
+		{
+			$this->sourcePath = __DIR__ . '/source';
+			$this->js = require(__DIR__ . '/files.php');
+		}
+		else
+		{
+			$this->sourcePath = __DIR__ . '/dist';
+			$this->js = require(__DIR__ . '/js-tools.min.js');
+		}
+
+		parent::init();
+	}
+}
