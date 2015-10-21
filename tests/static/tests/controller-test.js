c12d6c9ca397a051b0dc16a532dae36591e7e135
Sergiy Misyura <sergey@cando.com>
2015-10-20 21:07:49 -0400

    Basic controller tests

---
+++ tests/static/tests/controller-test.js
@@ -0,0 +1,28 @@
+
+buster.testCase('Controllers', {
+		setUp: function() {
+			this.app = window.app;
+
+			this.app.controller('test-controller', function() {
+				return {
+					init: function() {
+						this.initialized = true;
+					}
+				};
+			});
+		},
+
+		'Controller registered': function() {
+			buster.assert.isFunction(this.app._.controllerFactories['test-controller']);
+		},
+
+		'Controller created and initialized': function() {
+			var element = $('<div/>')
+				.attr('data-controller', 'test-controller')
+				.get(0);
+			this.app.createController(element);
+			buster.assert.isObject(element.controller);
+			buster.assert(element.controller.initialized);
+		}
+	}
+);
